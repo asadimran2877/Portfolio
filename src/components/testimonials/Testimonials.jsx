1ec6f6b55dad738aff2ccd9fa80ac2ab465de179
asadimran2877 <asadimran2877@gmail.com>
2023-01-19 18:08:32 +0500

    Second commit

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./testimonials.css";
 const Testimonials = () => {
-  return <div>testimonials</div>;
+  return <section id="testimonials">testimonials</section>;
 };
 
 export default Testimonials;
